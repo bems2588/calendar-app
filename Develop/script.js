f3a62dcf0b2b0db889ae0eafbb2c8fdcd580382d
Bruno Marcenaro <bems2588@gmail.com>
2022-03-23 21:22:23 -0400

    add grid

---
+++ Develop/script.js
@@ -0,0 +1,8 @@
+
+const currentDay = document.getElementById("currentDay");
+currentDay.innerHTML = moment().format("dddd MMM Do YY");
+
+const saveBtn = document.querySelectorAll(".saveBtn");
+
+
+
--- script.js
+++
@@ -1,7 +0,0 @@
-const currentDay = document.getElementById("currentDay");
-
-
-
-currentDay.moment().format("MMM Do YY"); 
-
-console.log('Hello');
