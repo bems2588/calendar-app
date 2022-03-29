2b7850ddcfc0854edf2576e92d156b60dd9add34
Bruno Marcenaro <bems2588@gmail.com>
2022-03-29 11:33:17 -0400

    add day-hour

--- Develop/script.js
+++ Develop/script.js
@@ -4,5 +4,27 @@ currentDay.innerHTML = moment().format("dddd MMM Do YY");
 
 const saveBtn = document.querySelectorAll(".saveBtn");
 
+var currentHour = moment().format('HH');
 
+$(".hour").each(function () {
+  rowHour = $(this).attr("id");
+})
+//extract the value and save it 
+//which fields are open and which ones are grey
 
+//set task in open 
+
+//event listener onClick
+
+//store value in a variable
+
+//disable box by turning it red
+
+//then work with local storage object
+
+//     ---------------
+
+//Check in class activities
+
+//activity number one event listener
+// module 5 script js - set and get items
